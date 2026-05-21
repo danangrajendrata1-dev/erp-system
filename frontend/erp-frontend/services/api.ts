d12a9372ea4b015837f5a15cf25ad45ff94312d5
danangrajendrata1-dev <danangrajendrata1@gmail.com>
2026-05-21 22:07:51 +0700

    Fix frontend API URL

--- frontend/erp-frontend/services/api.ts
+++ frontend/erp-frontend/services/api.ts
@@ -1,28 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-
-  baseURL: "http://localhost:8000",
-
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-api.interceptors.request.use(
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-  (config) => {
-
-    const token = localStorage.getItem(
-      "token"
-    );
-
-    if (token) {
-
-      config.headers.Authorization =
-        `Bearer ${token}`;
-    }
-
-    return config;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
-);
+  return config;
+});
 
 export default api;
