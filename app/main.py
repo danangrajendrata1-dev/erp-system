4a5796ff819d775c6ce2356470da8cb17cabcc3c
danangrajendrata1-dev <danangrajendrata1@gmail.com>
2026-05-21 22:21:11 +0700

    Fix CORS for Vercel domain

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://https://erp-system-ten-jade.vercel.app"
+        "https://erp-system-ten-jade.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
